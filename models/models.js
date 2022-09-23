03a2fb30f310a33d0ecb30fe054cf317d34c3c8a
kirill Iluschenko <kirill.i@nekidaem.ru>
2022-09-23 15:17:22 +0300

    added model of user

---
+++ models/models.js
@@ -0,0 +1,17 @@
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+
+
+
+
+module.exports = {
+    User
+}
